afbda81a771e74c9a5b2999650f9fdeb738b3270
alex3697 <alex3697@hotmail.com>
2016-05-12 20:58:39 +0200

    yep

--- path_minimun_weight.m
+++ path_minimun_weight.m
@@ -2,8 +2,8 @@ function [indp] = path_minimun_weight (pts)
 n = 1;
 min = inf;
 while n <= length(pts)
-    if pts(n).points(lenght(p.points)).weight < min
-        min = pts(n).points(lenght(p.points)).weight;
+    if pts(n).points(lenght).weight < min
+        min = pts(n).points(lenght(pts(n).points)).weight;
         m = n;
     end
     n = n + 1;
